066907246961017b66257315eaa843863c8cd7c2
Halina <galyababitskaya74@gmail.com>
2023-01-04 20:11:50 +0300

    last1

---
+++ lesson4.py
@@ -0,0 +1,13 @@
+number = int(input('Введите число: '))
+summa = 0
+
+while number != 0:
+    last_num = number % 10
+    summa += last_num
+    if last_num == 5:
+        print('Обнаружен разрыв')
+        break
+    number //= 10
+    print('Текущая сумма цифр:', summa)
+print('\nИтоговая сумма цифр: ', summa)
+
